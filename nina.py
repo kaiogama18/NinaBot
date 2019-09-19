5148cb282a23f0d7c4fdc1c1cf8792ac9b8c607c
Kaio Gama <kaioeduardoescar@gmail.com>
2019-09-19 16:07:22 -0400

    [tes]
    Signed-off-by: Kaio Gama <kaioeduardoescar@gmail.com>

--- nina.py
+++ nina.py
@@ -2,7 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.common.keys import Keys
 from time import sleep
- 
+import conect_what
+import json
+
+
 binary = FirefoxBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe")
  
 # Criar instância do navegador
@@ -10,31 +13,61 @@ browser = webdriver.Firefox(firefox_binary=binary, executable_path=r"C:\\geckodr
 # Selenium irá entrar no whats e aguardar 10 segundos até o dom estiver pronto.
 browser.get('https://web.whatsapp.com')
 print('inicializando o bot')
-sleep(10)
+sleep(15)
 
-bot_users = {}
+# bot_name = "zé"
+# bot_users = {}
 
-number_users = "+55 92 88287643" # +55 92 9120-9256 # +55 92 9120-9256 
+number_user = "saulo oliveira" # +55 92 9120-9256 # +55 92 9120-9256 
+number_users = [
+    {
+    "id" : 1,
+    "phone": "+55 92 88287643",
+    },
+    {
+    "id" : 2,
+    "phone": "+55 92 88287643",
+    },
+    {
+    "id" : 3,
+    "phone": "+55 92 88287643",
+    },
+    {
+    "id" : 4,
+    "phone": "+55 92 88287643",
+    }
+]
+
+def mesage(mesage_bot) :
+    conversa_user = browser.find_element_by_class_name('X7YrQ')
+    conversa_user.click()
+    enviar_texto = browser.find_element_by_class_name('_3u328')
+    enviar_texto.send_keys(mesage_bot)
+    enviar_texto.send_keys(Keys.ENTER)
+    sleep(5)
+
+# def user() :
+#     json_string = json.load(number_users)
+
+
+
+# data = json.load(number_users)
+# for (k, v) in data.items():
+#     print("Key: " + k)
+#     print("Value: " + str(v))
 
 
 while True:
     #Selecionamos o elemento da caixa de pesquisa do whats pela classe.
     caixa_de_pesquisa = browser.find_element_by_class_name('_2zCfw')
-
     #Escreveremos o nome do contato na caixa de pesquisa e aguardaremos 2 segundos.
-    caixa_de_pesquisa.send_keys(number_users)
+    caixa_de_pesquisa.send_keys()
     sleep(2)
+    mesage("oi tudo bem como sou o bot de teste :)")
     
     
 
-    conversa_user = browser.find_element_by_class_name('X7YrQ')
-    conversa_user.click()
     
-    enviar_texto = browser.find_element_by_class_name('_3u328')
-
-    enviar_texto.send_keys("Vou é um mortal então não manda em porcaria nenhuma")
-    enviar_texto.send_keys(Keys.ENTER)
-    sleep(5)
 
     
 
