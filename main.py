86afff994bd3c954f4f346188dab719b432b51c5
Kaio B. Gama <kaioeduardoescar@gmail.com>
2019-09-22 00:28:32 -0400

    [Test] -- Checked functionality
    Signed-off-by: Kaio B. Gama <kaioeduardoescar@gmail.com>

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 import conect_what
 import json
-# number_user = "saulo oliveira" # +55 92 9120-9256 # +55 92 9120-9256 
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://web.whatsapp.com'
+soup = BeautifulSoup(requests.get(url).content, 'html.parser')
+
 binary = FirefoxBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe")
 
 # Criar instância do navegador
@@ -14,43 +19,50 @@ with open("json/phones.json") as file:
     data = json.load(file)
 
 labels = []
+json_names = []
+bot_users = {}
 
 for intent in data["intents"]:
-    # for number_user in intent["phone"]:
+    if intent["name"] not in json_names:
+        json_names.append(intent["name"]) 
+
     if intent["phone"] not in labels:
-        labels.append(intent["phone"])
-    print('[Lepros] ---> Lista de telefone:')
-    # print(labels[0])
+        labels.append(intent["phone"]) 
 
 # Selenium irá entrar no whats e aguardar 10 segundos até o dom estiver pronto.
-browser.get('https://web.whatsapp.com')
-print('[Lepros] ---> inicializando o bot')
+browser.get(url)
 sleep(10)
 
-def mesage(mesage_bot) :
-    conversa_user = browser.find_element_by_class_name('X7YrQ')
-    conversa_user.click()
-    enviar_texto = browser.find_element_by_class_name('_3u328')
-    enviar_texto.send_keys(mesage_bot)
-    enviar_texto.send_keys(Keys.ENTER)
-    sleep(5)
+def send_mesage(name, response) :
 
-while True:
-    #Selecionamos o elemento da caixa de pesquisa do whats pela classe.
-    caixa_de_pesquisa = browser.find_element_by_class_name('_2zCfw')
-    #Escreveremos o nome do contato na caixa de pesquisa e aguardaremos 2 segundos.
-    caixa_de_pesquisa.send_keys(labels[0])
+    search_box = browser.find_element_by_class_name('_2zCfw')
+    search_box.send_keys(name)
+    print('[Lepros] ---> nome do contato vindo do json: ' + name)
     sleep(2)
-    mesage("Sou o bot Mané e meu criador desejamos a você um ótimo final de semana :)")
-    
-    
+    contact_box = browser.find_element_by_class_name('X7YrQ')
+    contact_name  = browser.find_element_by_class_name("_19RFN").text
+    # message = browser.find_elements_by_class_name("vW7d1")[-1]
+	# images = message.find_elements_by_class_name("_3v3PK")
+    print('[Lepros] ---> nome do contato : ' + contact_name )
 
-    
+    if name not in bot_users:
+        if contact_name == name:
+            bot_users[contact_name] = True
+            contact_box.click()
+            text_box = browser.find_element_by_class_name('_3u328')
+            text_box.send_keys(response + contact_name)
+            text_box.send_keys(Keys.ENTER)
+        else :
+            browser.find_element_by_class_name('_2heX1').click()
+            print('[Lepros] ---> nome do contato : ' 
+                + contact_name + 
+                " não é igual ao nome do contato vindo do json :" 
+                + name )    
+            print("não encontrou o nome")
+            
 
-    
-
-
-
+for item in json_names:
+    send_mesage(item, "OI sou o bot e funciona! I am alive  ")
 
 
 
